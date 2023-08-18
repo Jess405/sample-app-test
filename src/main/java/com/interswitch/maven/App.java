bb35076d43ec2fc0b1b30d7a08985c07d0428d1b
Corporate Admin <iswcorp@ISW-ENGR-OA9.Interswitchng.com>
2023-08-18 12:42:50 +0100

    sample-apptest

---
+++ src/main/java/com/interswitch/maven/App.java
@@ -0,0 +1,15 @@
+package com.interswitch.maven;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException {
+        Calculator.chooseOperation();
+    }
+}
